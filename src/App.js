c5e67d8ad64494f86919dea9c6db6f99a3c2301f
JeffRisberg <jrisberg@aol.com>
2019-03-21 22:21:59 -0700

    end lecture 28

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Auth, Hub} from "aws-amplify";
 import {AmplifyTheme, Authenticator} from "aws-amplify-react"
 import "./App.css";
 
@@ -8,18 +9,41 @@ class App extends React.Component {
         user: null
     };
 
-    compnentDidMount() {
+    componentDidMount() {
+        console.dir(AmplifyTheme);
+        this.getUserData();
+        Hub.listen('auth', this, 'onHubCapsule');
+    };
 
-    }
+    getUserData = async () => {
+        const user = await Auth.currentAuthenticatedUser();
+        user ? this.setState({user}) : this.setState({user: null})
+    };
+
+    onHubCapsule = capsule => {
+        switch (capsule.payload.event) {
+            case "signIn":
+                console.log("signed in");
+                this.getUserData();
+                break;
+            case "signUp":
+                console.log("signed up");
+                break;
+            case "signOut":
+                console.log("sign out");
+                break;
+            default:
+                return;
+        }
+    };
 
     render() {
-        const { user } = this.state;
+        const {user} = this.state;
 
         return !user ? (
-            <Authenticator theme={(theme)} />
-        ) :
-        )
-        return <div>App</div>;
+                <Authenticator theme={(theme)}/>
+            ) :
+            <div>App</div>;
     }
 }
 
