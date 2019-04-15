bcf4732122d959eae3205e73afdfb8da37e403a9
JeffRisberg <jrisberg@aol.com>
2019-04-14 23:49:00 -0700

    end of lecture 48

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -36,50 +36,10 @@ const PayButton = ({ product, userAttributes }) => {
             console.log({ ownerEmail });
             const result = await API.post("orderlambda", "/charge", {
                 body: {
-                    token,
-                    charge: {
-                        currency: stripeConfig.currency,
-                        amount: product.price,
-                        description: product.description
-                    },
-                    email: {
-                        customerEmail: userAttributes.email,
-                        ownerEmail,
-                        shipped: product.shipped
-                    }
+                    token
                 }
             });
             console.log({ result });
-            if (result.charge.status === "succeeded") {
-                let shippingAddress = null;
-                if (product.shipped) {
-                    shippingAddress = createShippingAddress(result.charge.source);
-                }
-                const input = {
-                    orderUserId: userAttributes.sub,
-                    orderProductId: product.id,
-                    shippingAddress
-                };
-                const order = await API.graphql(
-                    graphqlOperation(createOrder, { input })
-                );
-                console.log({ order });
-                Notification({
-                    title: "Success",
-                    message: `${result.message}`,
-                    type: "success",
-                    duration: 3000
-                });
-                setTimeout(() => {
-                    history.push("/");
-                    Message({
-                        type: "info",
-                        message: "Check your verified email for order details",
-                        duration: 5000,
-                        showClose: true
-                    });
-                }, 3000);
-            }
         } catch (err) {
             console.error(err);
             Notification.error({
